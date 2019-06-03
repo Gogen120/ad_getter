a66995ede44d1e476ead3e9e4ee5e209ca3cc6d3
Armand Kulsh <zenars85@mail.ru>
2019-06-03 13:18:28 +0300

    SI replace xpath page processing with python, fix constant names

--- ad_getter/ad_getter/spiders/ad.py
+++ ad_getter/ad_getter/spiders/ad.py
@@ -13,22 +13,23 @@ class AdSpider(CrawlSpider):
     """
     Spider that get all product pages from given domain
 
-    Command to run: scrapy crawl ad -o <file_name>.csv -t csv -a domain=<domain_name>
+    Command to run: scrapy crawl ad -o <file_name>.csv -t csv -a domain=<domain_name> -a start_url=<start_url>
     """
     name = 'ad'
 
     def __init__(self, *args, **kwargs):
         self.allowed_domains = [f'{kwargs.get("domain")}']
-        self.start_urls = [f'https://{kwargs.get("domain")}']
+        self.start_urls = [f'{kwargs.get("start_url")}']
 
         self.rules = [
-            Rule(LinkExtractor(deny=[r'compare', r'/tel:'], unique=True), callback='parse_item', follow=True)
+            Rule(LinkExtractor(allow=[r'{}'.format(self.start_urls[0])], deny=[r'compare', r'/tel:', r'\.php'], unique=True),
+                 callback='parse_item', follow=True)
         ]
 
         super().__init__(*args, **kwargs)
 
     def parse_item(self, response):
-        if (is_product_page(response, const.TAG_FIELD_MAP, const.PRODUCT_KEYWORDS) or
+        if (is_product_page(response, const.BUY_TAGS, const.BUY_KEYWORDS) or
             is_product_page(response, const.NOT_IN_SHOP_TAGS, const.NOT_IN_SHOP_KEYWORDS)) and (
             is_product_page(response, const.SIMILAR_PRODUCT_TAGS, const.SIMILAR_PRODUCT_KEYWORDS) or
             is_product_page(response, const.DESCRIPTION_TAGS, const.DESCRIPTION_KEYWORDS)
--- ad_getter/ad_getter/spiders/utils.py
+++ ad_getter/ad_getter/spiders/utils.py
@@ -4,20 +4,24 @@ from scrapy.exceptions import NotSupported
 from scrapy.http.response.html import HtmlResponse
 
 
-def is_page_contains_keyword(response: HtmlResponse, keyword: str, tags: dict) -> bool:
-    """Check if page contains given keyword in tag provided by tags argument"""
-    for tag, field in tags.items():
-        try:
-            if response.xpath(
-                f'//{tag}[(translate({field}, "{const.CHARS_TO_TRANSLATE}", "{const.CHARS_TO_TRANSLATE.lower()}") = "{keyword}")]'
-            ).get() is not None:
-                return True
-        except NotSupported:
-            return False
+def extract_matched_tags_values(response: HtmlResponse, tags: dict) -> list:
+    """Extract and clean all values that match given tags map"""
+    matched_values = [response.xpath(f'//{tag}/{field}').getall() for tag, field in tags.items()]
+    return [
+        value.lower().strip().replace(':', '') for values in matched_values
+        for value in values if value is not None
+    ]
+
+
+def is_product_page(response: HtmlResponse, tags: dict, keywords: tuple) -> bool:
+    """Check if page contains any of possible product page keywords"""
+    try:
+        matched_values = extract_matched_tags_values(response, tags)
+    except NotSupported:
+        return False
+
+    for keyword in keywords:
+        if keyword in matched_values:
+            return True
 
     return False
-
-
-def is_product_page(response: HtmlResponse, tags: dict, keywords: dict) -> bool:
-    """Check if page contains any of possible product page keywords"""
-    return any([is_page_contains_keyword(response, keyword, tags) for keyword in keywords])
